4cef3f487913e4cb7739219ef01cb6e2a59aecbb
Mostafa Abdelrahman <43328278+Mustafa-Abdelrahman@users.noreply.github.com>
2021-09-25 20:41:24 +0200

    Generic Repository Implementation

---
+++ EagleEye/DataAccess/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EagleEye.DataAccess.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(object Id);
+        void Add(T Entity);
+        void Update(T Entity);
+        void Delete(T Entity);
+    }
+}
---
+++ EagleEye/DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+namespace EagleEye.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly SqlDbContext.SqlDbContext _context;
+        private DbSet<T> entities;
+        private string ErrorMsg = string.Empty;
+        public Repository(SqlDbContext.SqlDbContext context)
+        {
+            this._context = context;
+            entities = context.Set<T>();
+        }
+
+        public T GetById(object Id)
+        {
+            if (Id == null) throw new ArgumentNullException("Id");
+
+            return entities.Find(Id);
+        }
+        public IEnumerable<T> GetAll()
+        {
+            return entities.AsEnumerable();
+        }
+
+        public void Add(T Entity)
+        {
+            if (Entity == null) throw new ArgumentNullException("Entity");
+
+            entities.Add(Entity);
+            _context.SaveChanges();
+        }
+
+        public void Delete(T Entity)
+        {
+            if (Entity == null) throw new ArgumentNullException("Entity");
+            entities.Remove(Entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T Entity)
+        {
+            if (Entity == null) throw new ArgumentNullException("Entity");
+            _context.SaveChanges();
+        }
+    }
+}
